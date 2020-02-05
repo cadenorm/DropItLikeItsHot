895f194172b9a618caf39ff410934bebf6511508
JeffreyND <jeffreyndimele@yahoo.com>
2020-02-05 16:11:25 -0600

    main meu

--- Project/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Project/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.textView2);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                textView.setText("Sorry, this button doesn't do anything yet!");
+            }
+        });
     }
 }
