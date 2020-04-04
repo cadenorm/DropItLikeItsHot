9ea89e446f42077494c86bab64d3123cdb0c531c
JeffreyND <jeffreyndimele@yahoo.com>
2020-04-04 10:55:06 -0500

    test1

---
+++ Project/app/src/main/java/com/example/myapplication/Game.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Game extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("tst", "hello");
+        setContentView(R.layout.game);
+    }
+
+}
