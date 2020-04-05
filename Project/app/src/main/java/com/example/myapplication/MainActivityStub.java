b4eff04a6658a44910cd7c43a76849976ab71d34
JeffreyND <jeffreyndimele@yahoo.com>
2020-04-05 18:02:14 -0500

    tests

--- Project/app/src/main/java/com/example/myapplication/MainActivityStub.java
+++ Project/app/src/main/java/com/example/myapplication/MainActivityStub.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
+
 public class MainActivityStub {
 
-    public int[][] createJBlock(){
-        int[][] gridFill = new int[7][9];
+    static boolean blockInMotion = false;
+    Piece blockType = Piece.J;
+
+    public int[][] createJBlock() {
+        int [][] gridFill = new int [7][9];
         gridFill[3][8] = 1;
         gridFill[3][7] = 1;
         gridFill[3][6] = 1;
@@ -11,5 +15,77 @@ public class MainActivityStub {
 
         return gridFill;
     }
+
+
+    public int [][] moveBlockDown(int [][] gridFill){
+        if(blockType.toString() == "J"){
+            //check if bottom cell:
+            for(int i = 0; i < 7; i++){
+                if(gridFill[i][0] == 1){
+                    lockBlockPosition(gridFill);
+                    blockInMotion = false;
+                    return gridFill;
+                }
+            }
+            //check if hit block:
+            for(int x = 0; x < 7; x++){
+                for(int y = 1; y < 9; y++){
+                    if(gridFill[x][y] == 1 && gridFill[x][y - 1] == 2){
+                        lockBlockPosition(gridFill);
+                        blockInMotion = false;
+                        return gridFill;
+                    }
+                }
+            }
+        }
+        for(int x = 1; x < 8; x++){
+            for(int y = 2; y < 10; y++) {
+
+                //Log.d("test", "test:" + x + ":" + y + "--" + gridFill[x - 1][y - 1]);
+                if(gridFill[x - 1][y - 1] != 1) continue;
+                //Log.d("test", "" + "b" + x + y)
+                gridFill[x - 1][y - 1] = 0;
+                gridFill[x - 1][y - 2] = 1;
+            }
+        }
+        return gridFill;
+    }
+    int [][] moveBlockLeft(int [][] gridFill){
+        for(int x = 1; x < 7; x++){
+            for(int y = 0; y < 9; y++){
+                if(gridFill[x][y] == 1){
+                    if(gridFill[x - 1][y] == 2) return gridFill;
+                    gridFill[x][y] = 0;
+                    gridFill[x - 1][y] = 1;
+                }
+            }
+        }
+        return gridFill;
+    }
+
+    int [][] moveBlockRight(int [][] gridFill){
+        for(int x = 5; x >= 0; x--){
+            for(int y = 0; y < 9; y++){
+                if(gridFill[x][y] == 1){
+                    if(gridFill[x + 1][y] == 2) return gridFill;
+                    gridFill[x][y] = 0;
+                    gridFill[x + 1][y] = 1;
+                }
+            }
+        }
+        return gridFill;
+    }
+
+    int [][] lockBlockPosition(int [][] gridFill){
+        for(int x = 0; x < 7; x++){
+            for(int y = 0; y < 9; y++){
+                if(gridFill[x][y] == 1){
+                    gridFill[x][y] = 2;
+                }
+            }
+        }
+        return gridFill;
+    }
+
 
 }
--- Project/app/src/test/java/com/example/myapplication/MainActivityTest.java
+++ Project/app/src/test/java/com/example/myapplication/MainActivityTest.java
@@ -25,17 +25,61 @@ public class MainActivityTest {
 
     @Test
     public void moveBlockDown() {
-    }
+        MainActivityStub stub = new MainActivityStub();
+        int [][] output = stub.createJBlock();
+        output = stub.moveBlockDown(output);
+        int [][] input = new int [7][9];
 
+        input[3][7] = 1;
+        input[3][6] = 1;
+        input[3][5] = 1;
+        input[3][4] = 1;
+
+        assertArrayEquals(input, output);
+
+    }
     @Test
-    public void moveBlockLeft() {
+    public void moveBlockLeft(){
+        MainActivityStub stub = new MainActivityStub();
+        int [][] output = stub.createJBlock();
+        output = stub.moveBlockLeft(output);
+        int [][] input = new int [7][9];
+
+        input[2][8] = 1;
+        input[2][7] = 1;
+        input[2][6] = 1;
+        input[2][5] = 1;
+
+        assertArrayEquals(input, output);
     }
 
     @Test
     public void moveBlockRight() {
+        MainActivityStub stub = new MainActivityStub();
+        int [][] output = stub.createJBlock();
+        output = stub.moveBlockRight(output);
+        int [][] input = new int [7][9];
+
+        input[4][8] = 1;
+        input[4][7] = 1;
+        input[4][6] = 1;
+        input[4][5] = 1;
+
+        assertArrayEquals(input, output);
     }
 
     @Test
     public void lockBlockPosition() {
+        MainActivityStub stub = new MainActivityStub();
+        int [][] output = stub.createJBlock();
+        output = stub.lockBlockPosition(output);
+        int [][] input = new int [7][9];
+
+        input[3][8] = 2;
+        input[3][7] = 2;
+        input[3][6] = 2;
+        input[3][5] = 2;
+
+        assertArrayEquals(input, output);
     }
 }
